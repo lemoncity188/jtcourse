7672f41a6e3f08739488acf4c0b9e1be5e1a29f1
weiweigong <weiweigong@osauto.com>
2017-01-27 13:59:34 +0800

    test

--- yii/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii',
-    'username' => 'root',
-    'password' => '你的密码',
-    'charset' => 'utf8',
-];
